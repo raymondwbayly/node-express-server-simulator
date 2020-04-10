8cfe195be8c4a7dfc43acce04e5f7c6d9b0bb0f8
Raymond Bayly <60951450+raymondbayly@users.noreply.github.com>
2020-04-10 16:20:39 -0400

    update index

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// this is the index
+// index.js
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+    res.status(200).send("Beowulf connection works");
+});
+
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
